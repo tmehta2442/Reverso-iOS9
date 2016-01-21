7693fb941ec5be664bd4c9fff04c1e4cd7c3c9db
Tejas Mehta <tmehta@apple.com>
2016-01-21 15:57:48 -0800

    finished app- connected to storyboard, clear and reverse working

--- Reverso iOS9/ViewController.swift
+++ Reverso iOS9/ViewController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+    
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var lblReversedWord: UILabel!
+    
+    var wordData = ""
+    var reversedWord = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnReverse(sender: UIButton) {
+        reverseTheWord()
+    }
 
+    @IBAction func btnClear(sender: UIButton) {
+        clear()
+    }
+    
+    func reverseTheWord(){
+        wordData = txtName.text!
+        reversedWord = String(wordData.characters.reverse())
+        hideKeyboard()
+        printReversedWord()
+    }
+    
+    func clear(){
+        txtName.text = ""
+        lblReversedWord.text = "Word"
+        hideKeyboard()
+    }
+    
+    func printReversedWord(){
+        lblReversedWord.text = "\(reversedWord.lowercaseString)"
+    }
+    
+    func hideKeyboard(){
+        txtName.resignFirstResponder()
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        hideKeyboard()
+    }
+    
 }
 
